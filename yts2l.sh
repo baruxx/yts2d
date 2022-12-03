af903c9061c01d067c135f8395e707a987c9eeec
BKR13 <95540800+baruxx@users.noreply.github.com>
2022-12-03 20:07:59 +0100

    Update yts2l.sh

--- yts2l.sh
+++ yts2l.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-function check_errors { if [ "$?" != "0" ]; then echo "Error" && exit -1; fi }
-
 read -p "Music/Author Name: " keyword
 if [ -z "$keyword" ]; then echo "Invalid keyword" && exit -1; fi
 
@@ -16,32 +14,30 @@ curl -s 'https://ytpp3.com/ytsearch' \
   -H 'uuid: 083c2452740957913c0acde7a74d1448' \
   --data-raw "{\"key_word\":\"$keyword\"}" \
   --compressed 2>/dev/null >/tmp/ytpp3
-check_errors
 
 track=0
 rm -f /tmp/ytpp3_urls 2>/dev/null
 cat /tmp/ytpp3 | jq -r ".result[] | [.title,.duration,.url] | @tsv" | while read music; do
 track=$((track+1)); echo "$music" | tr -d "," 2>/dev/null | sed 's/\t/,/g' 2>/dev/null | while read i; do
 echo -e "\x1b[41m# Track $track\x1b[0m" 2>/dev/null
-echo -e " Name: \x1b[1m\x1b[33m$(echo $i | cut -d, -f1 2>/dev/null)\x1b[0m" && check_errors
-echo " Duration: $(echo $i | cut -d, -f2 2>/dev/null)" && check_errors
-echo " URL: $(echo $i | cut -d, -f3 2>/dev/null)" && check_errors
-echo "$track $(echo $i | cut -d, -f3 2>/dev/null)" 2>/dev/null >>/tmp/ytpp3_urls && check_errors
+echo -e " Name: \x1b[1m\x1b[33m$(echo $i | cut -d, -f1 2>/dev/null)\x1b[0m"
+echo " Duration: $(echo $i | cut -d, -f2 2>/dev/null)"
+echo " URL: $(echo $i | cut -d, -f3 2>/dev/null)"
+echo "$track $(echo $i | cut -d, -f3 2>/dev/null)" 2>/dev/null >>/tmp/ytpp3_urls
 done
 done
-check_errors
 
 while :; do
 	read -p "Track Number (1-10 / 0=all): " ntrack
 	if [[ "$ntrack" =~ ^[1-9]$ ]] || [[ "$ntrack" == "10" ]]; then
-		link_to_play=$(cat /tmp/ytpp3_urls 2>/dev/null | grep "^$ntrack " 2>/dev/null| cut -d " " -f2 2>/dev/null); check_errors
+		link_to_play=$(cat /tmp/ytpp3_urls 2>/dev/null | grep "^$ntrack " 2>/dev/null| cut -d " " -f2 2>/dev/null)
 		echo "Playing Track $ntrack..."
-		vlc -I "dummy" "$link_to_play" --no-video --no-loop --no-repeat --play-and-exit 2>/dev/null 3>/dev/null >/dev/null; check_errors
+		vlc -I "dummy" "$link_to_play" --no-video --no-loop --no-repeat --play-and-exit 2>/dev/null 3>/dev/null >/dev/null
 	elif [[ "$ntrack" == "0" ]]; then
 		cat /tmp/ytpp3_urls | while read i; do
 			link_to_play=$(echo "$i" | cut -d " " -f2 2>/dev/null)
 			echo "Playing Track $(echo $i | cut -d ' ' -f1)..."
-			vlc -I "dummy" "$link_to_play" --no-video --no-loop --no-repeat --play-and-exit 2>/dev/null 3>/dev/null >/dev/null; check_errors
+			vlc -I "dummy" "$link_to_play" --no-video --no-loop --no-repeat --play-and-exit 2>/dev/null 3>/dev/null >/dev/null
 		done
 	else
 		echo "Unvailable Track Number"
